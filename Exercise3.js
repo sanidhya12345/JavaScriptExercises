0a81b3b4f7bf7b6166765a251d26cfd3e6ff8ea5
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-21 16:31:09 +0530

    exercise3

---
+++ Exercise3.js
@@ -0,0 +1,10 @@
+// Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
+function leapyear(year)
+{
+return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+}
+console.log(leapyear(2016));
+console.log(leapyear(2000));
+console.log(leapyear(1700));
+console.log(leapyear(1800));
+console.log(leapyear(100));
