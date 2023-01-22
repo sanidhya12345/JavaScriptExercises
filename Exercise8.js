0ec36b3f6ebe4da9cfbe6c53eb1783a488a06ff7
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-22 16:27:45 +0530

    exercise 8

---
+++ Exercise8.js
@@ -0,0 +1,7 @@
+//Write a JavaScript program to get the difference between a given number and 13, if the number is greater than 13 return double the absolute difference.
+
+var num=45;
+var diff=45-13;
+if(diff>13){
+    console.log(diff*2);
+}
