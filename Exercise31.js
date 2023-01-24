6e7a4e35a7a5f79f9f1500e4aa3987fc2feaf922
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-24 17:34:31 +0530

    exercise 31

---
+++ Exercise31.js
@@ -0,0 +1,26 @@
+//Write a JavaScript function that accepts a number as a parameter and check the number is prime or not.
+
+function test_prime(n)
+{
+
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+
+console.log(test_prime(37));
