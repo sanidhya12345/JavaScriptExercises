d0a0b2ca6afc41b747f2dc2ca618dfbac1e1374b
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-25 17:42:19 +0530

    exercise 36

---
+++ Exercise36.js
@@ -0,0 +1,22 @@
+//Write a JavaScript function to convert an amount to coins
+function amountTocoins(amount, coins) 
+{
+ if (amount === 0) 
+  {
+     return [];
+   } 
+ else
+   {
+     if (amount >= coins[0]) 
+       {
+        left = (amount - coins[0]);
+        return [coins[0]].concat( amountTocoins(left, coins) );
+        } 
+      else
+        {
+         coins.shift();
+         return amountTocoins(amount, coins);
+        }
+    }
+} 
+console.log(amountTocoins(46, [25, 10, 5, 2,1]));
