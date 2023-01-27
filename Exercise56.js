09f118917edbb8b7d0d921f4e5ed1b02ab5b702d
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-27 17:30:24 +0530

    recursion

---
+++ Exercise56.js
@@ -0,0 +1,24 @@
+//Write a JavaScript program to check whether a number is even or not
+function is_even_recursion(number)
+{
+  if (number < 0) 
+  {
+    number = Math.abs(number);
+  }
+  if (number===0) 
+  {
+    return true;
+  }
+  if (number===1) 
+  {
+    return false;
+  }
+  else 
+  {
+    number = number - 2;
+    return is_even_recursion(number);
+  }
+}
+console.log(is_even_recursion(234)); //true
+console.log(is_even_recursion(-45)); // false
+console.log(is_even_recursion(65)); // false
