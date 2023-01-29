1391f03ee8a29f7616dde11f5afcd06a38fab01c
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-29 17:07:27 +0530

    exercise 70

---
+++ Exercise70.js
@@ -0,0 +1,6 @@
+//Write a simple JavaScript program to join all elements of the following array into a string.
+
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join('+'));
