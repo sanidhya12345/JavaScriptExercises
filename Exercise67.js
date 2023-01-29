d72f583b3526169f993927114bf53afec8888e5c
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-29 17:03:35 +0530

    exercise 67

---
+++ Exercise67.js
@@ -0,0 +1,7 @@
+//Write a JavaScript function to clone an array.
+var array_Clone = function(arra1) {
+    return arra1.slice(0);
+       };
+   console.log(array_Clone([1, 2, 4, 0]));
+   console.log(array_Clone([1, 2, [4, 0]]));
+   
