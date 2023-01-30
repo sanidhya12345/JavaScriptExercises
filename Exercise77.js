be1bba4b9abc95cc8c0122826ac06d41692195b5
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-30 17:08:04 +0530

    exercise 77

---
+++ Exercise77.js
@@ -0,0 +1,5 @@
+//Write a JavaScript function to parameterize a string
+string_parameterize = function (str1) {
+    return str1.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
+};
+console.log(string_parameterize("Robin Singh from USA."));
