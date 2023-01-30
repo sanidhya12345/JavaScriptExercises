0233e90a149838eb1480576873237790098f6b89
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-01-30 17:09:33 +0530

    exercise 78

---
+++ Exercise78.js
@@ -0,0 +1,8 @@
+// Write a JavaScript function to capitalize the first letter of a string.
+
+capitalize = function(str1){
+    return str1.charAt(0).toUpperCase() + str1.slice(1);
+  }
+      
+   console.log(capitalize('js string exercises'));
+   
